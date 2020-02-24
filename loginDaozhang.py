05482efcf724e63fcdba1adc384f586edae5ad3d
zhangyiming748 <37614800+zhangyiming748@users.noreply.github.com>
2020-02-24 13:07:31 +0800

    实现模拟网页第三方登录

--- loginDaozhang.py
+++ loginDaozhang.py
@@ -1,10 +1,20 @@
 from GetSetMessages import send
 import sys
+def GUI():
+    print("选择一个功能")
+    print("1. Telegram登录道长ss网站")
+    cmd = input("输入序号")
+    if cmd =="q":
+        return
+    else:
+        if cmd =="1":
+            print("输入登录验证码")
+            key = int(input())
+            loginUserCenter(key)
 def loginUserCenter(keyword):
     text=str(keyword)
     url = "https://t.me/baipiaodaozhang_bot"
     send(url,text)
     print("Done")
 if __name__ == '__main__':
-    key = sys.argv[1]
-    loginUserCenter(str(key))
+    GUI()
